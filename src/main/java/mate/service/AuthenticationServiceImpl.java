1c7be3e34900be6a7ea360165708121e0ae2c260
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-10-20 17:03:30 +0300

    implemented taxi-service app

---
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -0,0 +1,29 @@
+package mate.service;
+
+import java.util.Optional;
+import mate.exception.AuthenticationException;
+import mate.lib.Inject;
+import mate.lib.Service;
+import mate.model.Driver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager
+            .getLogger(AuthenticationServiceImpl.class);
+    @Inject
+    private DriverService driverService;
+
+    @Override
+    public Driver login(String login, String password)
+            throws AuthenticationException {
+        logger.info(" Method login was called. User login: {}", login);
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isPresent() && driver.get().getPassword().equals(password)) {
+            return driver.get();
+        }
+        throw new AuthenticationException("Incorrect username or password");
+    }
+}
+
